197eed27043a2fe31a76bfd750262c09d46528e0
Wenjing Zeng <wenjingzeng@WenjingdeMacBook-Pro.local>
2026-03-23 10:17:44 +0800

    feat: add ngrok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true,
+    allowedHosts: ['jacelyn-isographical-lashonda.ngrok-free.dev', 'localhost'],
     proxy: {
       '/api': {
         target: `http://localhost:${process.env.BACKEND_PORT || 8001}`,
